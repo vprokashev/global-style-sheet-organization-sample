ef105c8648c5a8c677592a7677bcb5223d1b6e07
Prokashev Vadim <prokashevvs@gmail.com>
2017-02-18 16:44:32 +0400

    fs structure/add core rules

--- source/core/lib/index.js
+++ source/core/lib/index.js
@@ -1,9 +1,7 @@
 //https://github.com/ride-css/ride-css/tree/master/lib/ride-css/gs
 
 var stylus = require('stylus');
-// getting stylus helpers
 var helpers = stylus.helpers;
-// getting stylus nodes
 var nodes = stylus.nodes;
 
 // exporting the plugin
